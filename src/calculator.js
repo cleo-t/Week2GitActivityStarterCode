c7e164258ab516472679643ff8490a95c4f382c9
arunimag23 <emailarunimagupta@gmail.com>
2023-09-12 11:25:26 -0400

    add subtract operation

---
+++ src/calculator.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Calculator = void 0;
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.add = function (x, y) {
+        return x + y;
+    };
+    return Calculator;
+}());
+exports.Calculator = Calculator;
+;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var calculator_1 = require("./calculator");
+var calc = new calculator_1.Calculator();
+var result_add = calc.add(2, 3);
+var resutl_subtract = calc.subtract(7,5);
+console.log("result_add = ".concat(result_add));
+console.log("result_sub = ".concat(resutl_subtract));

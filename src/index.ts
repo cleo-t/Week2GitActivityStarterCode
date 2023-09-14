a2c734b6dc38fd311318f5083c85bdad2ec194b4
arunimag23 <emailarunimagupta@gmail.com>
2023-09-13 21:29:43 -0400

    print message when done

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 const result_subtract = calc.subtract(7, 5);
 const result_multiply = calc.multiply(2, 3);
+const result_divide = calc.divide(4,2)
 console.log(`result_add = ${result_add}`);
 console.log(`result_subtract = ${result_subtract}`);
 console.log(`result_multiply = ${result_multiply}`);
+console.log(`result_divide = ${result_divide}`);
+console.log('done.')
 
